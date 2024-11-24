4795c9c87f1c2b2efe07ba7a8d6f71b09125391f
dianaRodriguess <dianarodrigues044@gmail.com>
2024-11-23 21:44:34 -0300

    feat: imprime no lcd

--- main.ino
+++ main.ino
@@ -1,18 +1,29 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 //variavel do pino que esta plugado o Sensor
 //Neste caso é o pino 2, mais pode usar qualquer pino digital
 #define ONE_WIRE_BUS 2
 
-//Instacia o Objeto oneWire e Seta o pino do Sensor para iniciar as leituras
+//Instacia o Objeto oneWire e seta o pino do Sensor para iniciar as leituras
 OneWire oneWire(ONE_WIRE_BUS);
 
 //Repassa as referencias do oneWire para o Sensor Dallas (DS18B20)
 DallasTemperature sensor(&oneWire);
 
+void escreveTemperatura(float temp){
+  lcd.setCursor(0, 0);
+  lcd.print("Temperatura:");
+  lcd.setCursor(0, 1);
+  lcd.print(temp);
+}
 
 void setup() {
+  lcd.init();
+  lcd.setBacklight(HIGH);
   Serial.begin(9600);
   Serial.println("Sensor de temperatura Dallas DS18b20");
   //Inicia o objeto da biblioteca do Dallas
@@ -25,5 +36,22 @@ void loop() {
   float leitura = sensor.getTempCByIndex(0);
   //Imprime na serial a varivel que recebe os dados do Sensor
   Serial.println(leitura);
+  if (leitura < 25){
+    lcd.setCursor(0, 0);
+    lcd.print("Endotermica:");
+    lcd.setCursor(0, 1);
+    lcd.print(leitura);
+  } else if (leitura > 29){
+    lcd.setCursor(0, 0);
+    lcd.print("Exotermica:");
+    lcd.setCursor(0, 1);
+    lcd.print(leitura);
+  } else {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Normal:");
+    lcd.setCursor(0, 1);
+    lcd.print(leitura);
+  }
   delay(100);
 }
--- main.py
+++ main.py
@@ -1,23 +1,36 @@
+from pyfirmata import Arduino
 import serial
 import time
+import lcd
 
 # Configuração da porta serial (substitua por sua porta serial correta)
 porta_serial = '/dev/ttyUSB0'  # Use 'COM3' no Windows
 baud_rate = 9600  # Taxa de transmissão
+arduino = Arduino(porta_serial)
+
+lcd.escrever(arduino, 0, 0, 'Iniciando...')
 
 # Inicializa a conexão serial
 ser = serial.Serial(porta_serial, baud_rate)
-time.sleep(2)  # Aguarde para garantir que a conexão está estabelecida
+time.sleep(1)  # Aguarde para garantir que a conexão está estabelecida
 
 try:
     while True:
         if ser.in_waiting > 0:
             temperatura = ser.readline().decode('latin-1').strip()  # Lê e decodifica os dados
             print(f"Temperatura: {temperatura} °C")
-        time.sleep(1)
+            # liblcd.escreverTemp(Arduino(porta_serial), temperatura)
+        time.sleep(0.5)
+        # liblcd.escrever(Arduino(porta_serial), 0, 0, f'Temperatura: {temperatura} °C')
 
 except KeyboardInterrupt:
-    print("Programa interrompido pelo usuário.")
+    print("\nPrograma interrompido pelo usuário.")
+    arduino.exit()
 
+except Exception as e:
+    print(f"\nOcorreu um erro: {e}")
+    
 finally:
     ser.close()
+    
+
